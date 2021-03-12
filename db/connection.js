98ffa62c2d174c42049e2368db6510bd5d7c89b1
Nes <epmjj4@hotmail.com>
2021-03-12 16:44:36 -0500

    created all starter files and started working on schema file

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const util = require('util');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: process.env.password,
+    database:'emp_db'     
+});
+
+
+connection.connect();
+//using connection query to return an object using promises
+
+connection.query = util.promisify(connection.query); 
+module.exports =  connection;
+
